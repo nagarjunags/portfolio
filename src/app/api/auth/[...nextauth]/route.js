90c3eadf3bdc8aa11220b1f5e037f434c4c2537e
nagarjunags <nagarjunags2014@gmail.com>
2024-10-10 12:06:51 +0530

    test

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,5 +2,8 @@ import { authOptions } from "@/utils/authOptions";
 import NextAuth from "next-auth/next";
 
 const handler = NextAuth(authOptions);
-console.log("----------------------------");
+console.log(
+  process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+  "----------------------------"
+);
 export { handler as GET, handler as POST };
